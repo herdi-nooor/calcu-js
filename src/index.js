e7925fee5c4471c8c26986ad74ecb544d493455d
herdin nor <73617545+herdi-nooor@users.noreply.github.com>
2022-03-31 20:00:25 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ let prevNumber = "";
 let calculationOperator = "";
 let currentNumber = "";
 
+// key-number click function
 const numbers = document.querySelectorAll(".number");
 
 numbers.forEach((number) => {
   number.addEventListener("click", (event) => {
+    if (calculatorScreen.value === "0") {
+      currentNumber = "";
+    }
     inputNumber(event.target.value);
     updateScreen(currentNumber);
   });
@@ -17,6 +21,7 @@ const updateScreen = (number) => {
   calculatorScreen.value = number;
 };
 
+// input function
 const inputNumber = (number) => {
   if (currentNumber === "0") {
     currentNumber = number;
@@ -25,6 +30,7 @@ const inputNumber = (number) => {
   }
 };
 
+// input operator
 const operator = document.querySelectorAll(".operator");
 
 operator.forEach((operator) => {
@@ -36,11 +42,15 @@ operator.forEach((operator) => {
 const inputOperator = (operator) => {
   if (calculationOperator === "") {
     prevNumber = currentNumber;
+  } else {
+    calculate();
+    prevNumber = currentNumber;
   }
   calculationOperator = operator;
   currentNumber = "0";
 };
 
+// calculation event function
 const equalSign = document.querySelector(".equal-sign");
 
 equalSign.addEventListener("click", () => {
@@ -66,16 +76,20 @@ const calculate = () => {
     default:
       break;
   }
+  if (prevNumber === "0") {
+    currentNumber = "0";
+  }
   currentNumber = result;
   calculationOperator = "";
 };
 
+// clenner function
 const clearBtn = document.querySelector(".all-clear");
 
 const claerAll = () => {
   prevNumber = "";
   calculationOperator = "";
-  currentNumber = "";
+  currentNumber = "0";
 };
 
 clearBtn.addEventListener("click", () => {
@@ -83,6 +97,7 @@ clearBtn.addEventListener("click", () => {
   updateScreen(currentNumber);
 });
 
+// function for decimal value
 const decimal = document.querySelector(".decimal");
 const inputDecimal = (dot) => {
   if (currentNumber.includes(".")) {
@@ -94,4 +109,26 @@ const inputDecimal = (dot) => {
 decimal.addEventListener("click", (event) => {
   inputDecimal(event.target.value);
   updateScreen(currentNumber);
+});
+
+//zero value handling
+const dzero = document.querySelector(".double-zero-btn");
+
+dzero.addEventListener("click", () => {
+  if (calculatorScreen.value === "0") {
+    claerAll();
+    updateScreen("0");
+  } else {
+    inputNumber("00");
+    updateScreen(currentNumber);
+  }
+});
+
+const zero = document.querySelector(".zero-btn");
+
+zero.addEventListener("click", () => {
+  if (calculatorScreen.value === "0") {
+    claerAll();
+    updateScreen("0");
+  }
 });
